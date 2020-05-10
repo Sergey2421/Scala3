4b5627ed7e0a13424a6b6955619e0f39a714706e
Sergey2421 <61668850+Sergey2421@users.noreply.github.com>
2020-05-10 19:12:54 +0300

    Update Laba6.scala

--- src/main/scala/Laba6.scala
+++ src/main/scala/Laba6.scala
@@ -5,7 +5,9 @@ object Laba6 {
       val strList = List("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
       val tempstrlist = strList.sortBy(_.length)                                                               // part 1
       tempstrlist.foreach(System.out.println)                                                                  // part 1
+        
       System.out.println(strList.last)                                                                         // part 2
+        
       val str = "Trust me, I'm a programmer"                                                                   // part 3
       val tempstr = str.split(",")                                                                             // part 3
       tempstr.foreach(System.out.println)                                                                     // part 3
